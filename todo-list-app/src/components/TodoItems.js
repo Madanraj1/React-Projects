75478047c81e3198c847cea6da60b1caf1f66899
madanraj <madanraj251999@gmail.com>
2020-05-20 13:34:48 +0530

    tutorials

---
+++ todo-list-app/src/components/TodoItems.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import FlipMove from 'react-flip-move';
+
+class TodoItems extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+        this.createTask  = this.createTask.bind(this);
+        this.delete = this.delete.bind(this);
+    }
+
+    delete(key) {
+        this.props.delete(key);
+    }
+
+    createTask(item)  {
+        return <li 
+        onClick={ ()=> this.delete(item.key) }
+        key= {item.key} 
+        >{item.text} </li>
+    }
+    
+
+
+
+    render() {
+        var todoEntries = this.props.entries;
+        var listItems = todoEntries.map(this.createTask);
+
+        return (
+            <ul className="theList">
+
+            {/* for animation */}
+            <FlipMove duration={250} easing="ease-out">
+                {listItems}
+            </FlipMove>
+            
+            </ul>
+        );
+    }
+};
+
+export default TodoItems
