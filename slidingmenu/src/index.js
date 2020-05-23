800abd77e34d63b52d4a21cf705a08c417ce787a
madanraj <madanraj251999@gmail.com>
2020-05-23 15:33:42 +0530

    Sliding Menu in React

---
+++ slidingmenu/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Menu from './components/Menu';
+
+
+ReactDOM.render(
+  
+<Menu title='Code Future'/>
+  ,
+  document.getElementById('root')
+);
+
